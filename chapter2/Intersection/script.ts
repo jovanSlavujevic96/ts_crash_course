a4237edc73c7db09624fe3cf5164b068c4760ddd
jovanSlavujevic96 <jotzilla96@gmail.com>
2026-08-26 20:56:34 +0000

    Intersection

---
+++ chapter2/Intersection/script.ts
@@ -0,0 +1,31 @@
+type User = {
+    name: string;
+    age: number;
+    courses: string[];
+}
+
+type Permission = {
+    permissionLevel: string;
+}
+
+type UserExtended = User & Permission; // Intersection type
+
+const user1: User = {
+    name: 'Jan',
+    age: 27,
+    courses: ['C', 'C++', 'Python', 'Typescript'],
+}
+const user2: UserExtended = {
+    name: 'Daniel',
+    age: 24,
+    courses: ['Java', 'C#'],
+    permissionLevel: 'admin',
+}
+
+function printUser(user: User) {
+    console.log("user name: ",user.name);
+    console.log("user age: ", user.age);
+    console.log("user courses: ",user.courses);
+}
+printUser(user1)
+printUser(user2)
